dce853f2fcf7e48f181a1a150e42f3ad6be870cf
alfred82santa <alfred82santa@gmail.com>
2016-07-05 21:18:33 +0200

    New stuff

---
+++ Makefile
@@ -0,0 +1,61 @@
+
+PACKAGE_NAME = aiowerkzeug
+PACKAGE_COVERAGE = aiowerkzeug
+
+help:
+	@echo "Options"
+	@echo "-----------------------------------------------------------------------"
+	@echo "help:                     This help"
+	@echo "requirements:             Download requirements"
+	@echo "requirements-test:        Download requirements for tests"
+	@echo "requirements-docs:        Download requirements for docs"
+	@echo "run-tests:                Run tests with coverage"
+	@echo "publish:                  Publish new version on Pypi"
+	@echo "clean:                    Clean compiled files"
+	@echo "flake:                    Run Flake8"
+	@echo "prepush:                  Helper to run before to push to repo"
+	@echo "pull-request:             Helper to run before to merge a pull request"
+	@echo "autopep:                  Reformat code using PEP8"
+	@echo "-----------------------------------------------------------------------"
+
+requirements:
+	@echo "Installing ${PACKAGE_NAME} requirements..."
+	pip install -r requirements.txt
+
+requirements-test:
+	@echo "Installing ${PACKAGE_NAME} tests requirements..."
+	@make requirements
+	pip install -r requirements-test.txt
+
+requirements-docs:
+	@echo "Installing ${PACKAGE_NAME} docs requirements..."
+	@make requirements
+	pip install -r requirements-docs.txt
+
+run-tests:
+	@echo "Running tests..."
+	nosetests --with-coverage -d --cover-package=${PACKAGE_COVERAGE} --cover-erase
+
+publish:
+	@echo "Publishing new version on Pypi..."
+	python setup.py sdist upload
+
+clean:
+	@echo "Cleaning compiled files..."
+	find . | grep -E "(__pycache__|\.pyc|\.pyo)$ " | xargs rm -rf
+
+flake:
+	@echo "Running flake8 tests..."
+	flake8 ${PACKAGE_COVERAGE}
+	flake8 tests
+
+autopep:
+	autopep8 --max-line-length 120 -r -j 8 -i .
+
+prepush:
+	@make flake
+	@make run-tests
+
+pull-request:
+	@make flake
+	@make run-tests
--- tests/tests_local.py
+++ tests/tests_local.py
@@ -5,7 +5,8 @@ from asyncio.futures import Future
 from asyncio.tasks import Task
 from asynctest.case import TestCase
 from werkzeug.local import Local, LocalStack
-from aiowerkzeug.local import identify_future, patch_local, context_coroutine
+from aiowerkzeug.local import identify_future, patch_local, context_coroutine, AsyncLocal, AsyncLocalStack, \
+    async_task_with_context, keep_context_factory, AsyncLocalManager
 
 __author__ = 'alfred'
 
@@ -30,7 +31,7 @@ class IdentifyTest(TestCase):
         def other_task():
             return identify_future()
 
-        fut = asyncio.async(other_task())
+        fut = asyncio.ensure_future(other_task())
         yield from fut
         self.assertNotEqual(fut.result(), identify_future())
 
@@ -52,7 +53,7 @@ class PatchLocalTest(TestCase):
 
         ctx.test = 40
 
-        fut = asyncio.async(other_context())
+        fut = asyncio.ensure_future(other_context())
         yield from fut
         self.assertEqual(ctx.test, 40)
         self.assertNotEqual(ctx.test, fut.result())
@@ -71,11 +72,60 @@ class PatchLocalTest(TestCase):
 
         ctx.push(40)
 
-        fut = asyncio.async(other_context())
+        fut = asyncio.ensure_future(other_context())
         yield from fut
         self.assertEqual(ctx.top, 40)
         self.assertNotEqual(ctx.top, fut.result())
         self.assertEqual(fut.result(), 45)
+
+
+class AsyncLocalTest(TestCase):
+
+    use_default_loop = True
+
+    @coroutine
+    def test_coroutine_local(self):
+
+        ctx = AsyncLocal()
+
+        @coroutine
+        def other_context():
+            ctx.test = 45
+            return ctx.test
+
+        ctx.test = 40
+
+        fut = asyncio.ensure_future(other_context())
+        yield from fut
+        self.assertEqual(ctx.test, 40)
+        self.assertNotEqual(ctx.test, fut.result())
+        self.assertEqual(fut.result(), 45)
+
+        self.assertIn(identify_future(fut), ctx.__storage__)
+        ctx.__release_local__(fut)
+        self.assertNotIn(identify_future(fut), ctx.__storage__)
+
+    @coroutine
+    def test_coroutine_localstack(self):
+
+        ctx = AsyncLocalStack()
+
+        @coroutine
+        def other_context():
+            ctx.push(45)
+            return ctx.top
+
+        ctx.push(40)
+
+        fut = asyncio.ensure_future(other_context())
+        yield from fut
+        self.assertEqual(ctx.top, 40)
+        self.assertNotEqual(ctx.top, fut.result())
+        self.assertEqual(fut.result(), 45)
+
+        self.assertIn(identify_future(fut), ctx._local.__storage__)
+        ctx.__release_local__(fut)
+        self.assertNotIn(identify_future(fut), ctx._local.__storage__)
 
 
 class LocalContextCoroutineTest(TestCase):
@@ -108,7 +158,7 @@ class LocalContextCoroutineTest(TestCase):
         def other_context():
             return ctx.test
 
-        fut = asyncio.async(other_context())
+        fut = asyncio.ensure_future(other_context())
         yield from fut
         self.assertFalse(hasattr(ctx, 'test'))
         self.assertEqual(fut.result(), 45)
@@ -126,7 +176,7 @@ class LocalContextCoroutineTest(TestCase):
         def other_context():
             return ctx.test
 
-        fut = asyncio.async(other_context())
+        fut = asyncio.ensure_future(other_context())
         yield from fut
         self.assertFalse(hasattr(ctx, 'test'))
         self.assertEqual(fut.result(), 45)
@@ -148,7 +198,7 @@ class LocalContextCoroutineTest(TestCase):
         def other_context():
             return gen()
 
-        fut = asyncio.async(other_context())
+        fut = asyncio.ensure_future(other_context())
         yield from fut
         self.assertFalse(hasattr(ctx, 'test'))
         self.assertEqual(fut.result(), 45)
@@ -175,3 +225,134 @@ class LocalContextCoroutineTest(TestCase):
             gene.send(None)
         except StopIteration as ex:
             self.assertEqual(ex.value, 45)
+
+
+
+
+class LocalKeepContextFactoryTest(TestCase):
+
+    use_default_loop = True
+
+    class CtxManager:
+
+        def __init__(self, local):
+            self.local = local
+
+        def __enter__(self):
+            self.local.test = 45
+
+        def __exit__(self, exc_type, exc_val, exc_tb):
+            try:
+                del self.local.test
+            except AttributeError:
+                pass
+
+    @coroutine
+    def test_func_context_local(self):
+
+        ctx = Local()
+        patch_local(ctx)
+        ctxman = self.CtxManager(ctx)
+        test_coroutine = partial(keep_context_factory, ctx=lambda: ctxman)
+
+        @test_coroutine
+        async def other_context():
+            return ctx.test
+
+        fut = asyncio.ensure_future(other_context())
+        yield from fut
+        self.assertFalse(hasattr(ctx, 'test'))
+        self.assertEqual(fut.result(), 45)
+
+    @coroutine
+    def test_coroutine_context_local(self):
+
+        ctx = Local()
+        patch_local(ctx)
+        ctxman = self.CtxManager(ctx)
+        test_coroutine = partial(keep_context_factory, ctx=lambda: ctxman)
+
+        @test_coroutine
+        async def other_context():
+            return ctx.test
+
+        fut = asyncio.ensure_future(other_context())
+        yield from fut
+        self.assertFalse(hasattr(ctx, 'test'))
+        self.assertEqual(fut.result(), 45)
+
+
+class AsyncTaskWithContextTest(TestCase):
+
+    use_default_loop = True
+
+    class CtxManager:
+
+        def __init__(self, local):
+            self.local = local
+
+        def __enter__(self):
+            self.local.test = 45
+
+        def __exit__(self, exc_type, exc_val, exc_tb):
+            try:
+                del self.local.test
+            except AttributeError:
+                pass
+
+    async def test_coroutine_local(self):
+        ctx = AsyncLocal()
+        ctxman = self.CtxManager(ctx)
+        self.callback = False
+
+        async def other_context():
+            return ctx.test
+
+        def callback():
+            self.callback = True
+
+        fut = async_task_with_context(other_context(), ctx=lambda: ctxman,
+                                      callback=callback, loop=self.loop)
+
+        await fut
+
+        self.assertFalse(hasattr(ctx, 'test'))
+        self.assertEqual(fut.result(), 45)
+        self.assertTrue(self.callback)
+
+
+class AsyncLocalManagerTest(TestCase):
+
+    use_default_loop = True
+
+    class CtxManager:
+
+        def __init__(self, local):
+            self.local = local
+
+        def __enter__(self):
+            self.local.test = 45
+
+        def __exit__(self, exc_type, exc_val, exc_tb):
+            try:
+                del self.local.test
+            except AttributeError:
+                pass
+
+    async def test_make_task_with_ctx_factory(self):
+        local = AsyncLocal()
+        local_man = AsyncLocalManager(locals=[local])
+        ctxman = self.CtxManager(local)
+        self.callback = False
+
+        async def other_context():
+            return local.test
+
+        async_with_context = local_man.make_task_with_ctx_factory(ctx=lambda: ctxman, loop=self.loop)
+
+        fut = async_with_context(other_context())
+        await fut
+
+        self.assertFalse(hasattr(local, 'test'))
+        self.assertEqual(fut.result(), 45)
+        self.assertNotIn(identify_future(fut), local.__storage__)
